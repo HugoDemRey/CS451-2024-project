889f5a718aec66e26fde19707cc7ee9bda25491a
HugoDemRey <101871068+HugoDemRey@users.noreply.github.com>
2024-10-25 10:05:38 +0200

    Update Constants.java

--- template_java/src/main/java/cs451/Constants.java
+++ template_java/src/main/java/cs451/Constants.java
@@ -19,7 +19,12 @@ public class Constants {
     public static final int CONFIG_VALUE = 6;
 
     // indexes for PerfectLinks
-    public static final int STANDARD_MESSAGE_SIZE_BYTES = 1024;
+    public static final int MAXIMUM_PACKET_SITE_BYTES = 64 * 1024;
+    public static final int MAX_MESSAGES_PER_PACKET = 8;
     public static final String ACK = "ACK";
     public static final int MAX_RETRIES = 5; // Maximum number of retries
+    public static final int STANDARD_WINDOW_SIZE = 5000;
+    public static final int MAXIMUM_WINDOW_SIZE = 30000;
+    public static final int STANDARD_TIMEOUT = 700;
+    public static final int MAXIMUM_TIMEOUT = 5000;
 }
