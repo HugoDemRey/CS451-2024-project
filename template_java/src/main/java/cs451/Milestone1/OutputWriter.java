eb4744a849029b005c87d9966d93decaf40a5e73
HugoDemRey <101871068+HugoDemRey@users.noreply.github.com>
2024-11-25 14:48:17 +0100

    Update OutputWriter.java to make it thread-safe

--- template_java/src/main/java/cs451/Milestone1/OutputWriter.java
+++ template_java/src/main/java/cs451/Milestone1/OutputWriter.java
@@ -1,5 +1,4 @@
 package cs451.Milestone1;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,8 +7,9 @@ import java.nio.file.StandardOpenOption;
 import static cs451.Constants.MAX_BUFFER_SIZE;
 
 public class OutputWriter {
-    private StringBuilder buffer;
-    private String outputFilePath;
+    private final StringBuilder buffer;
+    private final String outputFilePath;
+    private final Object lock = new Object(); // Lock object for synchronization
 
     public OutputWriter(String outputFilePath) {
         this.outputFilePath = outputFilePath;
@@ -28,46 +28,52 @@ public class OutputWriter {
 
     /**
      * Adds a line to the buffer and writes to file if buffer size is exceeded
+     * Thread-safe implementation using synchronization
      */
-    public void addData(String data){
-
-        try {
-            buffer.append(data);
-            if (buffer.length() >= MAX_BUFFER_SIZE) {
-                writeToFile();
+    public void addData(String data) {
+        synchronized (lock) {
+            try {
+                buffer.append(data);
+                if (buffer.length() >= MAX_BUFFER_SIZE) {
+                    writeToFile();
+                }
+            } catch (IOException e) {
+                System.err.println("Error writing to file");
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            System.out.println("Error writing to file");
-            e.printStackTrace();
         }
-
-        
     }
 
+    /**
+     * Writes debug information to a separate debug file
+     * Thread-safe implementation using synchronization
+     */
     public void debug(String data) {
-        try {
-            BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFilePath + ".debug"), StandardOpenOption.APPEND);
-            writer.write(data);
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Error writing to debug file");
-            e.printStackTrace();
+        synchronized (lock) {
+            try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFilePath + ".debug"), StandardOpenOption.APPEND)) {
+                writer.write(data);
+            } catch (IOException e) {
+                System.err.println("Error writing to debug file");
+                e.printStackTrace();
+            }
         }
     }
 
-    
     /**
      * Forces any remaining buffer content to be written to file
+     * Thread-safe implementation using synchronization
      */
     public void flush() throws IOException {
-        System.out.println("Flushing Buffer");
-        System.out.println("    Buffer Length: " + buffer.length());
-        System.out.println("    Buffer Content:");
-        for (String line : buffer.toString().split("\n")) {
-            System.out.println("        " + line);
-        }
-        if (buffer.length() > 0) {
-            writeToFile();
+        synchronized (lock) {
+            System.out.println("Flushing Buffer");
+            System.out.println("    Buffer Length: " + buffer.length());
+            System.out.println("    Buffer Content:");
+            for (String line : buffer.toString().split("\n")) {
+                System.out.println("        " + line);
+            }
+            if (buffer.length() > 0) {
+                writeToFile();
+            }
         }
     }
 
@@ -80,11 +86,12 @@ public class OutputWriter {
 
     /**
      * Writes the buffer to the file and clears the buffer
+     * This method is called within synchronized blocks to ensure thread safety
      */
     private void writeToFile() throws IOException {
-        BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFilePath), StandardOpenOption.APPEND);
-        writer.write(buffer.toString());
-        writer.close();
-        buffer.setLength(0); // Clear buffer
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFilePath), StandardOpenOption.APPEND)) {
+            writer.write(buffer.toString());
+        }
+        buffer.setLength(0);
     }
 }
